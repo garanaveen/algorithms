3c18f6e388e60a82f99edcdb93d8e1d656e21455
Naveen Gara <garanaveen@gmail.com>
2016-03-27 20:11:12 -0500

    Added gtest files to the repo - Huge change

--- _Test/_InsertionSortTest/main.cpp
+++ _Test/_InsertionSortTest/main.cpp
@@ -12,7 +12,41 @@ using namespace std;
 //https://github.com/google/googletest/blob/master/googletest/docs/Primer.md
 //You can start from this boilerplate:
 
+namespace{
+
+    class InsertionSortTest : public ::testing::Test
+    {
+    protected:
+        InsertionSortTest()
+        {
+
+        }
+        virtual ~InsertionSortTest()
+        {
+
+        }
+        virtual void SetUp()
+        {
+            vi.push_back(3);
+            vi.push_back(2);
+            vi.push_back(1);
+        }
+
+        virtual void TearDown()
+        {
+
+        }
+
+        vector<int> vi;
+};
+    TEST_F(InsertionSortTest, FirstTest)
+    {
+//        EXPECT_EQ(1,1);
+    }
+}
+
 int main(int argc, char** argv)
 {
+//    ::testing::InitGoogleTest(&argc, argv);
     return 0;
 }
