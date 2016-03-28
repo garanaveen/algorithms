8caeb87ff69370ab0ff805cd0f647951bf2738a8
Naveen Gara <garanaveen@gmail.com>
2016-03-27 20:16:29 -0500

    testing framework working. One gtest added successfully.

--- _Test/_InsertionSortTest/main.cpp
+++ _Test/_InsertionSortTest/main.cpp
@@ -41,12 +41,12 @@ namespace{
 };
     TEST_F(InsertionSortTest, FirstTest)
     {
-//        EXPECT_EQ(1,1);
+        EXPECT_EQ(1,1);
     }
 }
 
 int main(int argc, char** argv)
 {
-//    ::testing::InitGoogleTest(&argc, argv);
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
