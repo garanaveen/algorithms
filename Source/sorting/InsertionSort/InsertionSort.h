4ebbaacc354caf58318ef93c26b0d58a26e1d246
garanaveen <garanaveen@gmail.com>
2016-04-22 22:41:58 -0500

    Uncrustify all c++ files.

--- Source/sorting/InsertionSort/InsertionSort.h
+++ Source/sorting/InsertionSort/InsertionSort.h
@@ -5,23 +5,21 @@
 using namespace std;
 
 template<class T>
-class InsertionSort
-{
+class InsertionSort {
 public:
-    void Sort(vector<T>& list)
-	{
+
+  void Sort(vector<T>& list)
+  {
     int length = list.size();
 
-    for(int i = 1; i < length ; i++)
-        for(int j = i; j> 0; j--)
-            if(list[j] < list[j-1]){
-                 int tmp = list[j-1];
-                 list[j-1] = list[j];
-                 list[j] = tmp;
-            }
-    return;
-	}
-
+    for (int i = 1; i < length; i++)
+      for (int j = i; j > 0; j--)
+        if (list[j] < list[j - 1]) {
+          int tmp = list[j - 1];
+          list[j - 1] = list[j];
+          list[j]     = tmp;
+        }
+  }
 };
 
-#endif //INSERTION_SORT_H
+#endif // INSERTION_SORT_H
--- _Test/_SortTest/main.cpp
+++ _Test/_SortTest/main.cpp
@@ -1,8 +1,8 @@
 #define NOMINMAX
 
 #ifdef _WIN32
-#include <windows.h>
-#endif
+# include <windows.h>
+#endif // ifdef _WIN32
 
 #include <vector>
 #include <stdlib.h>
@@ -10,116 +10,110 @@
 #include <sorting/MergeSort/MergeSort.h>
 
 #include <gtest/gtest.h>
-#include<algorithm>
+#include <algorithm>
 
 using namespace std;
 
-//Useful links.
-//https://github.com/google/googletest
-//https://github.com/google/googletest/blob/master/googletest/docs/Primer.md
-//You can start from this boilerplate:
+// Useful links.
+// https://github.com/google/googletest
+// https://github.com/google/googletest/blob/master/googletest/docs/Primer.md
+// You can start from this boilerplate:
 
-namespace{
+namespace {
+class SortTest : public ::testing::Test {
+protected:
 
-    class SortTest : public ::testing::Test
+  SortTest()
+  {}
+
+  virtual ~SortTest()
+  {}
+
+  virtual void SetUp()
+  {}
+
+  virtual void TearDown()
+  {
+    vi.clear();
+  }
+
+  void BuildVectorOfOfTwoElements()
+  {
+    vi.push_back(2);
+    vi.push_back(1);
+  }
+
+  void BuildVectorOfOfFourElements()
+  {
+    vi.push_back(3);
+    vi.push_back(4);
+    vi.push_back(1);
+    vi.push_back(2);
+  }
+
+  void BuildVectorOfManyElements()
+  {
+    for (int i = 0; i < 100; i++)
     {
-    protected:
-        SortTest()
-        {
+      vi.push_back(rand());
+    }
+  }
 
-        }
-
-        virtual ~SortTest()
-        {
-
-        }
-
-        virtual void SetUp()
-        {
-        }
-
-        virtual void TearDown()
-        {
-            vi.clear();
-        }
-
-        void BuildVectorOfOfTwoElements()
-        {
-            vi.push_back(2);
-            vi.push_back(1);
-        }
-
-        void BuildVectorOfOfFourElements()
-        {
-            vi.push_back(3);
-            vi.push_back(4);
-            vi.push_back(1);
-            vi.push_back(2);
-        }
-
-        void BuildVectorOfManyElements()
-        {
-            for (int i=0; i<100 ; i++)
-            {
-                vi.push_back(rand());
-            }
-        }
-
-        vector<int> vi;
+  vector<int> vi;
 };
 }
 
 TEST_F(SortTest, InsertionSortTwoElements)
 {
-    BuildVectorOfOfTwoElements();
-    InsertionSort<int> isort;
-    isort.Sort(vi);
-    EXPECT_EQ(1,vi.at(0));
-    EXPECT_EQ(2,vi.at(1));
+  BuildVectorOfOfTwoElements();
+  InsertionSort<int> isort;
+  isort.Sort(vi);
+  EXPECT_EQ(1, vi.at(0));
+  EXPECT_EQ(2, vi.at(1));
 }
-
 
 TEST_F(SortTest, InsertionSortManyElements)
 {
-    BuildVectorOfManyElements();
-    vector<int> expectedList = vi;
-    std::sort(expectedList.begin(), expectedList.end());
-    InsertionSort<int> isort;
-    isort.Sort(vi);
-    EXPECT_EQ(vi, expectedList);
+  BuildVectorOfManyElements();
+  vector<int> expectedList = vi;
+  std::sort(expectedList.begin(), expectedList.end());
+  InsertionSort<int> isort;
+  isort.Sort(vi);
+  EXPECT_EQ(vi, expectedList);
 }
 
 TEST_F(SortTest, MergeSortTwoElements)
 {
-    BuildVectorOfOfTwoElements();
-    MergeSort<int> msort;
-    msort.Sort(vi);
-    EXPECT_EQ(1,vi.at(0));
-    EXPECT_EQ(2,vi.at(1));
+  BuildVectorOfOfTwoElements();
+  MergeSort<int> msort;
+  msort.Sort(vi);
+  EXPECT_EQ(1, vi.at(0));
+  EXPECT_EQ(2, vi.at(1));
 }
 
 TEST_F(SortTest, MergeSortFourElements)
 {
-    BuildVectorOfOfFourElements();
-    MergeSort<int> msort;
-    msort.Sort(vi);
-    EXPECT_EQ(1,vi.at(0));
-    EXPECT_EQ(2,vi.at(1));
+  BuildVectorOfOfFourElements();
+  MergeSort<int> msort;
+  msort.Sort(vi);
+  EXPECT_EQ(1, vi.at(0));
+  EXPECT_EQ(2, vi.at(1));
 }
 
 
 TEST_F(SortTest, MergeSortManyElements)
 {
-    BuildVectorOfManyElements();
-    vector<int> expectedList = vi;
-    std::sort(expectedList.begin(), expectedList.end());
-    MergeSort<int> msort;
-    msort.Sort(vi);
-    EXPECT_EQ(expectedList, vi);
+  BuildVectorOfManyElements();
+  vector<int> expectedList = vi;
+  std::sort(expectedList.begin(), expectedList.end());
+  MergeSort<int> msort;
+  msort.Sort(vi);
+  EXPECT_EQ(expectedList, vi);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
 }
