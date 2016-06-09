db9ab89a7f6803eb21c918a6356fec6aa1ec8d53
garanaveen <garanaveen@gmail.com>
2016-06-08 19:05:20 -0500

    RodCutting problem - Compiling. No tests added.

--- Source/DynamicProgramming/RodCutting/_unit/main.cpp
+++ Source/DynamicProgramming/RodCutting/_unit/main.cpp
@@ -10,8 +10,13 @@ protected:
     {}
 };
 
-
 TEST_F(RodCuttingTest, Test1)
 {
     EXPECT_EQ(1,1);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
