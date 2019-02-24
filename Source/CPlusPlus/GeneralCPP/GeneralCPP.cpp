0e2a442c5ba2172908083b42ace3aba35abe6b31
garanaveen <garanaveen@gmail.com>
2019-02-24 09:54:43 -0600

    Compiler warning.

--- Source/CPlusPlus/GeneralCPP/GeneralCPP.cpp
+++ Source/CPlusPlus/GeneralCPP/GeneralCPP.cpp
@@ -5,9 +5,11 @@
 int GeneralCPP::FunctionWithStdOut()
 {
    std::cout << "GeneralCPP::FunctionWithStdOut()" << std::endl;
+   return 0;
 }
 
 int GeneralCPP::FunctionWithStdErr()
 {
    std::cerr << "GeneralCPP::FunctionWithStdErr()" << std::endl;
+   return 0;
 }
