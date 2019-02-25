e3ee1e2a1a8fce25f1b831f92bca6539f383c63c
Naveen Gara <garanaveen@johndeere.com>
2019-02-25 11:06:08 -0600

    Refactored!

--- Source/CodeKatas/SupermarketQueue/SupermarketQueue.cpp
+++ Source/CodeKatas/SupermarketQueue/SupermarketQueue.cpp
@@ -1,43 +1,40 @@
 #include "SupermarketQueue.h"
-#include<iostream>
-#include <vector>
-#include <map>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-void SupermarketQueue::InitializeCurrentQueue(int size)
+void SupermarketQueue::InitializeCheckoutTills(int size)
 {
-    CurrentQueueStatus.reserve(size);
-    CurrentQueueStatus.resize(size);
+    CurrentCheckoutTills.reserve(size);
+    CurrentCheckoutTills.resize(size);
     // Is there a better way to initialize vector of ints to 0s?
-    std::vector<int>::iterator it = CurrentQueueStatus.begin();
-    for(; it != CurrentQueueStatus.end(); it++)
+    std::vector<int>::iterator it = CurrentCheckoutTills.begin();
+    for(; it != CurrentCheckoutTills.end(); it++)
     {
 	*it = 0;
     }
 }	
 
-void SupermarketQueue::InsertToQueue(int i)
+void SupermarketQueue::InsertCustomerToCheckoutTill(int i)
 {
-    //Insert in to CurrentQueue;
-    std::vector<int>::iterator min = std::min_element(CurrentQueueStatus.begin(), CurrentQueueStatus.end()); 
+    std::vector<int>::iterator min = std::min_element(CurrentCheckoutTills.begin(), CurrentCheckoutTills.end()); 
     //    std::cout << "i:" << i << ", min : " << *min << std::endl;
     *min += i;
 }
 
-int SupermarketQueue::CalculateTime(const std::vector<int>& queue, int numberOfQueues)
+int SupermarketQueue::CalculateTime(const std::vector<int>& queue, int numberOfCheckoutTills)
 {
-    InitializeCurrentQueue(numberOfQueues);
-    //    std::cout << "vector-size : " << CurrentQueueStatus.size() << std::endl;
+    InitializeCheckoutTills(numberOfCheckoutTills);
+    //    std::cout << "vector-size : " << CurrentCheckoutTills.size() << std::endl;
 
     int queueTime = 0 ;
     for (auto i : queue)
     {
-	InsertToQueue(i);
-	//queueTime += i;
-
+	InsertCustomerToCheckoutTill(i);
     }
 
-    std::vector<int>::iterator max = std::max_element(CurrentQueueStatus.begin(), CurrentQueueStatus.end()); 
+    std::vector<int>::iterator max = std::max_element(CurrentCheckoutTills.begin(), CurrentCheckoutTills.end()); 
     //    std::cout << "max : " << *max;
     return *max;
 }
+
--- Source/CodeKatas/SupermarketQueue/SupermarketQueue.h
+++ Source/CodeKatas/SupermarketQueue/SupermarketQueue.h
@@ -1,15 +1,14 @@
 #pragma once
 
-
 #include <vector>
 
 class SupermarketQueue
 {
     public:
-	int CalculateTime(const std::vector<int>& queue, int numberOfQueues);
+	int CalculateTime(const std::vector<int>& queue, int numberOfCheckoutTills);
     private:
-	void InitializeCurrentQueue(int size);
-	void InsertToQueue(int i);
-	std::vector<int> CurrentQueueStatus;
+	void InitializeCheckoutTills(int size);
+	void InsertCustomerToCheckoutTill(int i);
+	std::vector<int> CurrentCheckoutTills;
 };
 
