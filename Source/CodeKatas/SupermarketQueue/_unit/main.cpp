a679106ba1bdd4dcfa6a26e7cec118263449d2be
Naveen Gara <garanaveen@johndeere.com>
2019-02-25 11:09:37 -0600

    Refactored SupermarketQueue tests.

--- Source/CodeKatas/SupermarketQueue/_unit/main.cpp
+++ Source/CodeKatas/SupermarketQueue/_unit/main.cpp
@@ -14,58 +14,54 @@
 using namespace std;
 
 namespace {
-class SupermarketQueueTest : public ::testing::Test {
-protected:
+    class SupermarketQueueTest : public ::testing::Test {
+	protected:
 
-  SupermarketQueueTest()
-  {}
+	    SupermarketQueueTest()
+	    {}
 
-  virtual ~SupermarketQueueTest()
-  {}
+	    virtual ~SupermarketQueueTest()
+	    {}
 
-};
+    };
 }
 
-TEST_F(SupermarketQueueTest, OneQueueShouldJustAddUpAllTheCustomerTimes)
+TEST_F(SupermarketQueueTest, Test1)
 {
     SupermarketQueue sq;
-    constexpr const int numberOfQueues = 1;
-    std::vector<int> singlequeue = {1,2,3};
-    EXPECT_EQ(6, sq.CalculateTime(singlequeue, numberOfQueues));
+    constexpr const int numberOfCheckoutTills = 1;
+    std::vector<int> queue = {1,2,3};
+    EXPECT_EQ(6, sq.CalculateTime(queue, numberOfCheckoutTills));
 }
 
-
-TEST_F(SupermarketQueueTest, SimpleCaseForTwoQueues)
+TEST_F(SupermarketQueueTest, Test2)
 {
     SupermarketQueue sq;
-    constexpr const int numberOfQueues = 2;
+    constexpr const int numberOfCheckoutTills = 2;
     std::vector<int> queue = {2, 3, 4, 5};
-    EXPECT_EQ(8, sq.CalculateTime(queue, numberOfQueues));
+    EXPECT_EQ(8, sq.CalculateTime(queue, numberOfCheckoutTills));
 }
 
 TEST_F(SupermarketQueueTest, Test3)
 {
     SupermarketQueue sq;
-    constexpr const int numberOfQueues = 2;
+    constexpr const int numberOfCheckoutTills = 2;
     std::vector<int> queue = {10,2,3,3};
-    EXPECT_EQ(10, sq.CalculateTime(queue, numberOfQueues));
+    EXPECT_EQ(10, sq.CalculateTime(queue, numberOfCheckoutTills));
 }
 
 TEST_F(SupermarketQueueTest, Test4)
 {
     SupermarketQueue sq;
-    constexpr const int numberOfQueues = 2;
+    constexpr const int numberOfCheckoutTills = 2;
     std::vector<int> queue = {2,3,10};
-    EXPECT_EQ(12, sq.CalculateTime(queue, numberOfQueues));
+    EXPECT_EQ(12, sq.CalculateTime(queue, numberOfCheckoutTills));
 }
-
-
-
-
 
 int main(int argc, char **argv)
 {
-  ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
 
-  return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 }
+
